387155891081b863c4c7b0f79de8281828bb5f22
Shradheya Thakre <tshradheya@gmail.com>
2020-03-01 07:48:09 +0800

    Commands testing

--- test/sg/edu/nus/comp/cs4218/impl/cmd/PipeCommandTest.java
+++ test/sg/edu/nus/comp/cs4218/impl/cmd/PipeCommandTest.java
@@ -1,8 +1,10 @@
 package sg.edu.nus.comp.cs4218.impl.cmd;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
+import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_INVALID_REP_RULE;
+import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_SYNTAX;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -19,11 +21,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import sg.edu.nus.comp.cs4218.Environment;
+import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
+import sg.edu.nus.comp.cs4218.exception.ShellException;
+import sg.edu.nus.comp.cs4218.impl.app.TestUtils;
 import sg.edu.nus.comp.cs4218.impl.util.ApplicationRunner;
 import sg.edu.nus.comp.cs4218.impl.util.ArgumentResolver;
 
 /**
- * Tests for Pipe operator used in commands
+ * Tests for Pipe operator used in combining commands
  */
 public class PipeCommandTest {
 
@@ -56,23 +61,68 @@ public class PipeCommandTest {
     }
 
     /**
-     * Test to filter out only files which have filtered in their name
+     * Asserts if error message printed when ShellException thrown
      */
     @Test
-    public void testLsWithGrepUsingPipe() {
-        try {
-            CallCommand lsCommand = new CallCommand(new ArrayList<>(Collections.singleton("ls")), new ApplicationRunner(), new ArgumentResolver());
-            CallCommand grepCommand = new CallCommand(new ArrayList<>(Arrays.asList("grep", "filtered")), new ApplicationRunner(), new ArgumentResolver());
+    public void testThrowsShellExceptionForEmptyPipedCommand() {
+        CallCommand invalidCmd = new CallCommand(new ArrayList<>(), new ApplicationRunner(), new ArgumentResolver());
+        CallCommand echoCmd = new CallCommand(Arrays.asList("echo", "hi"), new ApplicationRunner(), new ArgumentResolver());
 
-            pipeCommand = new PipeCommand(new ArrayList<>(Arrays.asList(lsCommand, grepCommand)));
-            pipeCommand.evaluate(System.in, stdout);
+        pipeCommand = new PipeCommand(Arrays.asList(invalidCmd, echoCmd));
 
-            assertTrue(stdout.toString().contains("filtered.txt"));
-            assertFalse(stdout.toString().contains("random.txt"));
+        Exception exception = assertThrows(ShellException.class, () -> pipeCommand.evaluate(System.in, stdout));
+        TestUtils.assertMsgContains(exception, ERR_SYNTAX);
+    }
 
-        } catch (Exception e) {
-            fail("should not fail" + e);
-        }
+    /**
+     * Asserts if error message printed when AbstractApplicationException thrown
+     */
+    @Test
+    public void testThrowsCmdExceptionForInvalidCmd() {
+        CallCommand invalidSedCmd = new CallCommand(Arrays.asList("sed", "s|he|"), new ApplicationRunner(), new ArgumentResolver());
+        CallCommand echoCmd = new CallCommand(Arrays.asList("echo", "hi"), new ApplicationRunner(), new ArgumentResolver());
+
+        pipeCommand = new PipeCommand(Arrays.asList(invalidSedCmd, echoCmd));
+
+        Exception exception = assertThrows(AbstractApplicationException.class, () -> pipeCommand.evaluate(System.in, stdout));
+        TestUtils.assertMsgContains(exception, ERR_INVALID_REP_RULE);
+    }
+
+    /**
+     * Test to filter out only files which have filtered in their name
+     *
+     * @throws AbstractApplicationException
+     * @throws ShellException
+     */
+    @Test
+    public void testLsWithGrepUsingPipe() throws AbstractApplicationException, ShellException {
+        CallCommand lsCommand = new CallCommand(new ArrayList<>(Collections.singleton("ls")), new ApplicationRunner(), new ArgumentResolver());
+        CallCommand grepCommand = new CallCommand(new ArrayList<>(Arrays.asList("grep", "filtered")), new ApplicationRunner(), new ArgumentResolver());
+
+        pipeCommand = new PipeCommand(new ArrayList<>(Arrays.asList(lsCommand, grepCommand)));
+        pipeCommand.evaluate(System.in, stdout);
+
+        assertTrue(stdout.toString().contains("filtered.txt"));
+        assertFalse(stdout.toString().contains("random.txt"));
+
+    }
+
+    /**
+     * Test to echo a string by piping the input
+     *
+     * @throws AbstractApplicationException
+     * @throws ShellException
+     */
+    @Test
+    public void testEchoCommandsWithPipe() throws AbstractApplicationException, ShellException {
+        CallCommand echoCmd1 = new CallCommand(new ArrayList<>(Collections.singleton("echo")), new ApplicationRunner(), new ArgumentResolver());
+        CallCommand echoCmd2 = new CallCommand(new ArrayList<>(Arrays.asList("echo", "hello")), new ApplicationRunner(), new ArgumentResolver());
+
+        pipeCommand = new PipeCommand(new ArrayList<>(Arrays.asList(echoCmd1, echoCmd2)));
+        pipeCommand.evaluate(System.in, stdout);
+
+        assertTrue(stdout.toString().contains("hello"));
+
     }
 
 }
--- test/sg/edu/nus/comp/cs4218/impl/cmd/SequenceCommandTest.java
+++ test/sg/edu/nus/comp/cs4218/impl/cmd/SequenceCommandTest.java
@@ -6,7 +6,10 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import sg.edu.nus.comp.cs4218.Environment;
+import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
 import sg.edu.nus.comp.cs4218.exception.ExitException;
+import sg.edu.nus.comp.cs4218.exception.ShellException;
+import sg.edu.nus.comp.cs4218.impl.app.TestUtils;
 import sg.edu.nus.comp.cs4218.impl.util.ApplicationRunner;
 import sg.edu.nus.comp.cs4218.impl.util.ArgumentResolver;
 import sg.edu.nus.comp.cs4218.impl.util.StringUtils;
@@ -19,6 +22,7 @@ import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_SYNTAX;
 
 /**
  * Tests commands used in sequence
@@ -55,21 +59,6 @@ public class SequenceCommandTest {
     }
 
     /**
-     * Test to echo two different strings
-     */
-    @Test
-    public void testSameCommandsInSequence() throws Exception {
-        CallCommand echoCommand1 = new CallCommand(new ArrayList<>(Arrays.asList("echo", "hello")), new ApplicationRunner(), new ArgumentResolver());
-        CallCommand echoCommand2 = new CallCommand(new ArrayList<>(Arrays.asList("echo", "hi")), new ApplicationRunner(), new ArgumentResolver());
-
-        sequenceCommand = new SequenceCommand(new ArrayList<>(Arrays.asList(echoCommand1, echoCommand2)));
-        sequenceCommand.evaluate(System.in, stdout);
-
-        assertTrue(stdout.toString().contains("hello\nhi"));
-
-    }
-
-    /**
      * Throws exit exception if one of them is exit command
      */
     @Test
@@ -82,6 +71,36 @@ public class SequenceCommandTest {
 
         assertTrue(stdout.toString().contains("hi"));
         assertTrue(exception.toString().contains("terminating"));
+
+    }
+
+    /**
+     * Asserts if exception thrown when invalid commands are piped
+     * @throws AbstractApplicationException
+     * @throws ShellException
+     */
+    @Test
+    public void testThrowsShellExceptionForEmptyPipedCommand() throws AbstractApplicationException, ShellException {
+        CallCommand invalidCmd = new CallCommand(new ArrayList<>(), new ApplicationRunner(), new ArgumentResolver());
+
+        sequenceCommand = new SequenceCommand(Arrays.asList(invalidCmd));
+
+        sequenceCommand.evaluate(System.in, stdout);
+        assertTrue(stdout.toString().contains(ERR_SYNTAX));
+    }
+
+    /**
+     * Test to echo two different strings
+     */
+    @Test
+    public void testSameCommandsInSequence() throws Exception {
+        CallCommand echoCommand1 = new CallCommand(new ArrayList<>(Arrays.asList("echo", "hello")), new ApplicationRunner(), new ArgumentResolver());
+        CallCommand echoCommand2 = new CallCommand(new ArrayList<>(Arrays.asList("echo", "hi")), new ApplicationRunner(), new ArgumentResolver());
+
+        sequenceCommand = new SequenceCommand(new ArrayList<>(Arrays.asList(echoCommand1, echoCommand2)));
+        sequenceCommand.evaluate(System.in, stdout);
+
+        assertTrue(stdout.toString().contains("hello" + StringUtils.STRING_NEWLINE + "hi"));
 
     }
 }
