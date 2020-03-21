17da5dc66e0b54367d6843c955e8b73184a98a6d
tanhengyeow <E0032242@u.nus.edu>
2020-03-22 00:55:35 +0800

    Add more tests

--- test/integration/QuoteShellIntegrationTest.java
+++ test/integration/QuoteShellIntegrationTest.java
@@ -1,21 +1,53 @@
 package integration;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import sg.edu.nus.comp.cs4218.Environment;
 import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
 import sg.edu.nus.comp.cs4218.exception.ShellException;
 import sg.edu.nus.comp.cs4218.impl.ShellImpl;
+import sg.edu.nus.comp.cs4218.impl.util.StringUtils;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static sg.edu.nus.comp.cs4218.impl.util.StringUtils.STRING_NEWLINE;
 
 public class QuoteShellIntegrationTest {
+    public static final String ORIGINAL_DIR = Environment.getCurrentDirectory();
+
     private ShellImpl shell = new ShellImpl();
     private String cmdline = "";
     private String expected = "";
     private ByteArrayOutputStream output = new ByteArrayOutputStream();;
+
+    @BeforeAll
+    static void setupAll() {
+        Environment.setCurrentDirectory(ORIGINAL_DIR
+                + StringUtils.fileSeparator() + "dummyTestFolder"
+                + StringUtils.fileSeparator() + "IntegrationTestFolder"
+                + StringUtils.fileSeparator() + "QuoteShellFolder");
+
+    }
+
+    @AfterAll
+    static void reset() {
+        Environment.setCurrentDirectory(ORIGINAL_DIR);
+    }
+
+    @AfterEach
+    public void resetCurrentDirectory() throws IOException {
+        Environment.setCurrentDirectory(ORIGINAL_DIR
+                + StringUtils.fileSeparator() + "dummyTestFolder"
+                + StringUtils.fileSeparator() + "IntegrationTestFolder"
+                + StringUtils.fileSeparator() + "QuoteShellFolder");
+
+        output.flush();
+    }
 
     @Test
     public void testDoubleQuoteOutputRedirection()
@@ -186,4 +218,65 @@ public class QuoteShellIntegrationTest {
         shell.parseAndEvaluate(cmdline, output);
         assertEquals(expected, output.toString());
     }
+
+    @Test
+    public void testDoubleQuoteWithCut()
+            throws ShellException, AbstractApplicationException {
+        expected = "qe" + STRING_NEWLINE;
+        cmdline = "cut -c 1,8 \"quoteshelltest.txt\"";
+
+        shell.parseAndEvaluate(cmdline, output);
+        assertEquals(expected, output.toString());
+    }
+
+    @Test
+    public void testSingleQuoteWithCut()
+            throws ShellException, AbstractApplicationException {
+        expected = "qe" + STRING_NEWLINE;
+        cmdline = "cut -c 1,8 \'quoteshelltest.txt\'";
+
+        shell.parseAndEvaluate(cmdline, output);
+        assertEquals(expected, output.toString());
+    }
+
+    @Test
+    public void testDoubleQuoteWithDiff()
+            throws ShellException, AbstractApplicationException {
+        expected = "";
+        cmdline = "diff \"quoteshelltest.txt\" \"quoteshelltest-identical.txt\"";
+
+        shell.parseAndEvaluate(cmdline, output);
+        assertEquals(expected, output.toString());
+    }
+
+    @Test
+    public void testSingleQuoteWithDiff()
+            throws ShellException, AbstractApplicationException {
+        expected = "";
+        cmdline = "diff \'quoteshelltest.txt\' \'quoteshelltest-identical.txt\'";
+
+        shell.parseAndEvaluate(cmdline, output);
+        assertEquals(expected, output.toString());
+    }
+
+    @Test
+    public void testDoubleQuoteWithWc()
+            throws ShellException, AbstractApplicationException {
+        expected = "       0 quoteshelltest.txt" + STRING_NEWLINE;
+        cmdline = "wc -l \"quoteshelltest.txt\"";
+
+        shell.parseAndEvaluate(cmdline, output);
+        assertEquals(expected, output.toString());
+    }
+
+    @Test
+    public void testSingleQuoteWithWc()
+            throws ShellException, AbstractApplicationException {
+        expected = "       0 quoteshelltest.txt" + STRING_NEWLINE;
+        cmdline = "wc -l \'quoteshelltest.txt\'";
+
+        shell.parseAndEvaluate(cmdline, output);
+        assertEquals(expected, output.toString());
+    }
+
 }
