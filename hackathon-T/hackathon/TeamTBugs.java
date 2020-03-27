b3d29c1d02dc6033e76d49b13cd71add7b408c9c
Shradheya Thakre <tshradheya@gmail.com>
2020-03-27 12:42:41 +0800

    assertion

--- hackathon-T/hackathon/TeamTBugs.java
+++ hackathon-T/hackathon/TeamTBugs.java
@@ -1,9 +1,15 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -12,7 +18,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import com.sun.tools.doclint.Env;
+
+import sg.edu.nus.comp.cs4218.Environment;
 import sg.edu.nus.comp.cs4218.impl.ShellImpl;
+import sg.edu.nus.comp.cs4218.impl.util.IOUtils;
 import sg.edu.nus.comp.cs4218.impl.util.StringUtils;
 
 public class TeamTBugs {
@@ -51,7 +61,7 @@ public class TeamTBugs {
             shell.parseAndEvaluate("echo abc `echo 1 2 3`xyz`echo 4 5 6`", output);
             assertEquals("abc 1 2 3xyz4 5 6" + StringUtils.STRING_NEWLINE, output.toString());
         } catch (Exception e) {
-            fail();
+            fail(e.getMessage());
         }
     }
 }
