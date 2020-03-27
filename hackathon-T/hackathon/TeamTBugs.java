9092cb70033fd4389721bff93e72990873a8cee9
Pedro Teixeira <pedro.h.d.teixeira@gmail.com>
2020-03-27 11:35:58 +0000

    added test for cp with multiple files and one of them is nonexistent

--- hackathon-T/hackathon/TeamTBugs.java
+++ hackathon-T/hackathon/TeamTBugs.java
@@ -1,29 +1,24 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.fail;
-
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import com.sun.tools.doclint.Env;
-
 import sg.edu.nus.comp.cs4218.Environment;
+import sg.edu.nus.comp.cs4218.exception.CpException;
 import sg.edu.nus.comp.cs4218.impl.ShellImpl;
+import sg.edu.nus.comp.cs4218.impl.app.CpApplication;
 import sg.edu.nus.comp.cs4218.impl.util.IOUtils;
 import sg.edu.nus.comp.cs4218.impl.util.StringUtils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_FILE_NOT_FOUND;
 
 public class TeamTBugs {
 
@@ -57,11 +52,58 @@ public class TeamTBugs {
     @DisplayName("Bug Number #2")
     public void testNestedQuotesMultiple() {
         try {
-
+        
             shell.parseAndEvaluate("echo abc `echo 1 2 3`xyz`echo 4 5 6`", output);
             assertEquals("abc 1 2 3xyz4 5 6" + StringUtils.STRING_NEWLINE, output.toString());
         } catch (Exception e) {
             fail(e.getMessage());
         }
+    }
+    
+    /**
+     * cp with multiple files ignores all following files when there is an error with one of them
+     * The expected behavior would be for that error to be reported and the remaining files to be copied correctly
+     * This test assumes the error is reported through an exception and not to stdout
+     */
+    @Test
+    @DisplayName("Bug Number #13")
+    public void testCpMultipleFilesOneFails() throws IOException {
+        
+        // set curred dir to the folder with test assets
+        Environment.currentDirectory += StringUtils.fileSeparator() + "dummyTestFolder"
+          + StringUtils.fileSeparator() + "CpTestFolder";
+        
+        Path src1 = IOUtils.resolveFilePath("src1");
+        Path nonexistent = IOUtils.resolveFilePath("nonexistent");
+        Path src2 = IOUtils.resolveFilePath("src2");
+        Path destDir = IOUtils.resolveFilePath("destDir");
+        
+        // delete any leftover files from previous failes test runs
+        Files.deleteIfExists(destDir.resolve(src1));
+        Files.deleteIfExists(destDir.resolve(src2));
+        
+        String[] args = {src1.toString(), nonexistent.toString(), src2.toString(), destDir.toString()};
+        CpApplication cpApp = new CpApplication();
+        
+        CpException cpException = assertThrows(CpException.class, () -> cpApp.run(args, System.in, System.out));
+        
+        // ensure the right error is reported
+        assertTrue(cpException.getMessage().contains(ERR_FILE_NOT_FOUND));
+        
+        // ensure initial files still exist
+        assertTrue(Files.exists(src1));
+        assertTrue(Files.exists(src2));
+        
+        // ensure the nonexistent file was not created
+        assertFalse(Files.exists(nonexistent));
+        
+        // ensure existing files were correctly copied
+        assertTrue(Files.exists(destDir.resolve(src1)));
+        assertTrue(Files.exists(destDir.resolve(src2)));
+        
+        // clean up
+        Files.deleteIfExists(destDir.resolve(src1));
+        Files.deleteIfExists(destDir.resolve(src2));
+        
     }
 }
