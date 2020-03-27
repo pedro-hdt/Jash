e8554404fe6f52566f8c612f06ec594947abcef6
Pedro Teixeira <pedro.h.d.teixeira@gmail.com>
2020-03-27 15:59:18 +0000

    team U bug #5

--- hackathon-U/hackathon/TeamUBugs.java
+++ hackathon-U/hackathon/TeamUBugs.java
@@ -6,11 +6,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import sg.edu.nus.comp.cs4218.Environment;
 import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
+import sg.edu.nus.comp.cs4218.exception.CpException;
 import sg.edu.nus.comp.cs4218.exception.RmException;
 import sg.edu.nus.comp.cs4218.exception.ShellException;
 import sg.edu.nus.comp.cs4218.impl.ShellImpl;
+import sg.edu.nus.comp.cs4218.impl.app.CpApplication;
 import sg.edu.nus.comp.cs4218.impl.app.RmApplication;
 import sg.edu.nus.comp.cs4218.impl.util.IOUtils;
+import sg.edu.nus.comp.cs4218.impl.util.StringUtils;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -20,6 +23,8 @@ import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TeamUBugs {
     
@@ -67,7 +72,9 @@ public class TeamUBugs {
     @Test
     @DisplayName("Bug Number #1.1")
     public void testRmNonEmptyFolderRecursive() throws IOException, RmException {
-        
+    
+        Files.deleteIfExists(IOUtils.resolveFilePath("CS4218-rmTest"));
+    
         // create 4 levels of nested temporary directories with a temp file at each level
         // ignore refs to files because they should be deleted by rm
         Path[] testDirs = new Path[4];
@@ -97,19 +104,68 @@ public class TeamUBugs {
     @Test
     @DisplayName("Bug Number #1.2")
     public void testRmEmptyFolderRecursive() throws IOException, RmException {
-        
+    
+        Files.deleteIfExists(IOUtils.resolveFilePath("CS4218-rmTest"));
+    
         // create a temporary directory
         Path testDir = Files.createDirectory(IOUtils.resolveFilePath("CS4218-rmTest"));
-        
+    
         RmApplication rmApp = new RmApplication();
-        
+    
         // assemble args and call rm to delete the directory recursively
         String[] args = {"-r", testDir.toString()};
         rmApp.run(args, System.in, System.out);
-        
+    
         // make sure directory no longer exists afterwards
         assertFalse(Files.exists(testDir));
+    
+    }
+    
+    
+    /**
+     * cp copies a file to the same directory it is in, overwriting itself unnecessarily
+     * Should throw an exception reporting src and dest are the same file like GNU cp
+     */
+    @Test
+    @DisplayName("Bug #5.1")
+    public void testCpFailsSingleFileToSameDir() {
+        
+        // set curred dir to the folder with test assets
+        Environment.currentDirectory += StringUtils.fileSeparator() + "dummyTestFolder"
+          + StringUtils.fileSeparator() + "CpTestFolder";
+        
+        String[] args = {"src1", Environment.currentDirectory};
+        CpApplication cpApp = new CpApplication();
+        CpException cpException =
+          assertThrows(CpException.class, () -> cpApp.run(args, System.in, System.out));
+        
+        // In UNIX cp prints "<FILE> and <FILE> are the same file" so we assume this replicates such behavior
+        assertTrue(cpException.getMessage().contains("same file"));
         
     }
+    
+    
+    /**
+     * cp copies file into itself with similar behavior
+     * Should throw an exception reporting src and dest are the same file like GNU cp
+     */
+    @Test
+    @DisplayName("Bug #5.2")
+    public void testCpFailsSingleFileToItself() {
+        
+        // set curred dir to the folder with test assets
+        Environment.currentDirectory += StringUtils.fileSeparator() + "dummyTestFolder"
+          + StringUtils.fileSeparator() + "CpTestFolder";
+        
+        String[] args = {"src1", "src1"};
+        CpApplication cpApp = new CpApplication();
+        CpException cpException =
+          assertThrows(CpException.class, () -> cpApp.run(args, System.in, System.out));
+        
+        // In UNIX cp prints "<FILE> and <FILE> are the same file" so we assume this replicates such behavior
+        assertTrue(cpException.getMessage().contains("same file"));
+        
+    }
+    
     
 }
