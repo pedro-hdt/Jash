1554efff3abe79027b2226122375d07c60ec6568
tejas bhuwania <tejasbhuwania2805@gmail.com>
2020-03-28 17:13:38 +0800

    add tests

--- hackathon-U/hackathon/TeamUBugs.java
+++ hackathon-U/hackathon/TeamUBugs.java
@@ -8,12 +8,14 @@ import org.junit.jupiter.api.Test;
 import sg.edu.nus.comp.cs4218.Environment;
 import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
 import sg.edu.nus.comp.cs4218.exception.CpException;
+import sg.edu.nus.comp.cs4218.exception.FindException;
 import sg.edu.nus.comp.cs4218.exception.MvException;
 import sg.edu.nus.comp.cs4218.exception.PasteException;
 import sg.edu.nus.comp.cs4218.exception.RmException;
 import sg.edu.nus.comp.cs4218.exception.ShellException;
 import sg.edu.nus.comp.cs4218.impl.ShellImpl;
 import sg.edu.nus.comp.cs4218.impl.app.CpApplication;
+import sg.edu.nus.comp.cs4218.impl.app.FindApplication;
 import sg.edu.nus.comp.cs4218.impl.app.PasteApplication;
 import sg.edu.nus.comp.cs4218.impl.app.RmApplication;
 import sg.edu.nus.comp.cs4218.impl.util.ArgumentResolver;
@@ -31,6 +33,8 @@ import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static sg.edu.nus.comp.cs4218.TestUtils.assertMsgContains;
+import static sg.edu.nus.comp.cs4218.impl.app.FindApplication.MULTIPLE_FILES;
+import static sg.edu.nus.comp.cs4218.impl.app.FindApplication.WRONG_FLAG_SUFFIX;
 import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_IS_DIR;
 import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_NO_ISTREAM;
 import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_NO_OSTREAM;
@@ -38,21 +42,21 @@ import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_SYNTAX;
 import static sg.edu.nus.comp.cs4218.impl.util.StringUtils.STRING_NEWLINE;
 
 public class TeamUBugs {
-    
+
     ShellImpl shell = new ShellImpl();
     ByteArrayOutputStream output = new ByteArrayOutputStream();
     ByteArrayInputStream input;
     static String ORIGINAL_DIR;
-    
+
     @BeforeAll
     static void setupAll() {
         ORIGINAL_DIR = Environment.currentDirectory;
     }
-    
+
     @AfterAll
     static void reset() {
     }
-    
+
     @BeforeEach
     public void setup() {
         shell = new ShellImpl();
@@ -64,17 +68,17 @@ public class TeamUBugs {
         output.reset();
     }
 
-    
-    
+
+
     /**
      * rm with -r flag will not remove non-empty directories. Prints “This is a directory”
      */
     @Test
     @DisplayName("Bug Number #1.1")
     public void testRmNonEmptyFolderRecursive() throws IOException, RmException {
-    
+
         Files.deleteIfExists(IOUtils.resolveFilePath("CS4218-rmTest"));
-    
+
         // create 4 levels of nested temporary directories with a temp file at each level
         // ignore refs to files because they should be deleted by rm
         Path[] testDirs = new Path[4];
@@ -84,43 +88,43 @@ public class TeamUBugs {
             testDirs[i] = Files.createTempDirectory(testDirs[i - 1], "rmTest");
             Files.createTempFile(testDirs[i], "rmTest", "");
         }
-        
+
         Path testTree = testDirs[0];
-        
+
         RmApplication rmApp = new RmApplication();
-        
+
         // assemble args and call rm to delete the outer directory recursively
         String[] args = {"-r", testTree.toString()};
         rmApp.run(args, System.in, System.out);
-        
+
         // make sure directory no longer exists afterwards
         assertFalse(Files.exists(testTree));
-        
+
     }
-    
+
     /**
      * rm with -r flag will not remove empty directories. Prints “This is a directory”
      */
     @Test
     @DisplayName("Bug Number #1.2")
     public void testRmEmptyFolderRecursive() throws IOException, RmException {
-    
+
         Files.deleteIfExists(IOUtils.resolveFilePath("CS4218-rmTest"));
-    
+
         // create a temporary directory
         Path testDir = Files.createDirectory(IOUtils.resolveFilePath("CS4218-rmTest"));
-    
+
         RmApplication rmApp = new RmApplication();
-    
+
         // assemble args and call rm to delete the directory recursively
         String[] args = {"-r", testDir.toString()};
         rmApp.run(args, System.in, System.out);
-    
+
         // make sure directory no longer exists afterwards
         assertFalse(Files.exists(testDir));
-    
+
     }
-    
+
     /**
      * ‘rm -rd .’ removes all files in the current directory, which should not be possible
      */
@@ -128,14 +132,14 @@ public class TeamUBugs {
     @DisplayName("Bug #2")
     @Disabled("RUN WITH CAUTION: may delete everything in curr dir")
     public void testRmFailsDirEndInDot() {
-        
+
         RmApplication rmApp = new RmApplication();
-        
+
         RmException exception = assertThrows(RmException.class, () -> {
             rmApp.run(new String[]{"-rd", "."}, System.in, System.out);
         });
         assertTrue(exception.getMessage().contains("'.' or '..'")); // verify the correct exceptions is thrown
-        
+
     }
 
     /**
@@ -155,8 +159,8 @@ public class TeamUBugs {
         assertEquals("f1.txt" + StringUtils.STRING_NEWLINE + "match1.txt" + StringUtils.STRING_NEWLINE
                 + "mvFile.txt" + StringUtils.STRING_NEWLINE + "test.txt" + StringUtils.STRING_NEWLINE, output.toString());
     }
-    
-    
+
+
     /**
      * Double quotes interprets back quotes
      * CommandSubs within Double quotes adds extra spaces
@@ -166,11 +170,11 @@ public class TeamUBugs {
     @DisplayName("Bug #4")
     public void testDoubleQuotesWithBackQuote() throws AbstractApplicationException, ShellException {
         shell.parseAndEvaluate("echo \"This is random:`echo \"random\"`.\"", output);
-        
+
         assertEquals(output.toString(), "This is random:random.");
     }
-    
-    
+
+
     /**
      * cp copies a file to the same directory it is in, overwriting itself unnecessarily
      * Should throw an exception reporting src and dest are the same file like GNU cp
@@ -178,22 +182,22 @@ public class TeamUBugs {
     @Test
     @DisplayName("Bug #5.1")
     public void testCpFailsSingleFileToSameDir() {
-        
+
         // set curred dir to the folder with test assets
         Environment.currentDirectory += StringUtils.fileSeparator() + "dummyTestFolder"
           + StringUtils.fileSeparator() + "CpTestFolder";
-        
+
         String[] args = {"src1", Environment.currentDirectory};
         CpApplication cpApp = new CpApplication();
         CpException cpException =
           assertThrows(CpException.class, () -> cpApp.run(args, System.in, System.out));
-        
+
         // In UNIX cp prints "<FILE> and <FILE> are the same file" so we assume this replicates such behavior
         assertTrue(cpException.getMessage().contains("same file"));
-        
+
     }
-    
-    
+
+
     /**
      * cp copies file into itself with similar behavior
      * Should throw an exception reporting src and dest are the same file like GNU cp
@@ -201,19 +205,19 @@ public class TeamUBugs {
     @Test
     @DisplayName("Bug #5.2")
     public void testCpFailsSingleFileToItself() {
-        
+
         // set curred dir to the folder with test assets
         Environment.currentDirectory += StringUtils.fileSeparator() + "dummyTestFolder"
           + StringUtils.fileSeparator() + "CpTestFolder";
-        
+
         String[] args = {"src1", "src1"};
         CpApplication cpApp = new CpApplication();
         CpException cpException =
           assertThrows(CpException.class, () -> cpApp.run(args, System.in, System.out));
-    
+
         // In UNIX cp prints "<FILE> and <FILE> are the same file" so we assume this replicates such behavior
         assertTrue(cpException.getMessage().contains("same file"));
-    
+
     }
 
 
@@ -241,25 +245,25 @@ public class TeamUBugs {
         }
 
     }
-    
-    
+
+
     /**
      * cp with null output stream displays Null Pointer Exception without specifying the cause
      */
     @Test
     @DisplayName("Bug #7")
     public void testCpFailsNullOutputStream() {
-        
+
         CpApplication cpApp = new CpApplication();
-        
+
         CpException cpException =
           assertThrows(CpException.class, () -> cpApp.run(new String[0], System.in, null));
-        
+
         assertTrue(cpException.getMessage().contains(ERR_NO_OSTREAM));
-        
+
     }
-    
-    
+
+
     /**
      * cp directory into itself creates infinite recursive nesting
      * Should skip the directory and report that it is the same file
@@ -267,15 +271,15 @@ public class TeamUBugs {
     @Test
     @DisplayName("Bug #8")
     public void testCpFolderToItselfWithAnotherValidFile() throws IOException {
-        
+
         String fileB = "fileB.txt";
         String dirA = "dirA";
-        
+
         CpApplication cpApp = new CpApplication();
-        
+
         Files.createFile(IOUtils.resolveFilePath(fileB));
         Files.createDirectory(IOUtils.resolveFilePath(dirA));
-        
+
         try {
             String[] args = {dirA, fileB, dirA};
             cpApp.run(args, System.in, System.out);
@@ -290,43 +294,43 @@ public class TeamUBugs {
               + StringUtils.fileSeparator() + dirA));
             Files.deleteIfExists(Paths.get(Environment.currentDirectory, fileB));
         }
-    
+
     }
-    
-    
+
+
     /**
      * paste with null input stream displays Null Pointer Exception without specifying the cause
      */
     @Test
     @DisplayName("Bug #9.1")
     public void testPasteNullInputStream() {
-        
+
         PasteApplication pasteApp = new PasteApplication();
-        
+
         PasteException exception =
           assertThrows(PasteException.class, () -> pasteApp.run(new String[0], null, output));
-        
+
         assertTrue(exception.getMessage().contains(ERR_NO_ISTREAM));
-        
+
     }
-    
+
     /**
      * paste with null output stream displays Null Pointer Exception without specifying the cause
      */
     @Test
     @DisplayName("Bug #9.2")
     public void testPasteNullOutputStream() {
-        
+
         PasteApplication pasteApp = new PasteApplication();
-        
+
         PasteException exception =
           assertThrows(PasteException.class, () -> pasteApp.run(new String[0], System.in, null));
-        
+
         assertTrue(exception.getMessage().contains(ERR_NO_OSTREAM));
-        
+
     }
-    
-    
+
+
     /**
      * Incorrect error message when calling paste with a directory as argument.
      * “No such file or directory” implies the user provided a nonexistent file.
@@ -334,21 +338,21 @@ public class TeamUBugs {
     @Test
     @DisplayName("Bug #10")
     public void testPasteDirOnlyFiles() throws IOException {
-        
+
         PasteApplication pasteApp = new PasteApplication();
-        
+
         Files.deleteIfExists(IOUtils.resolveFilePath("dir"));
         Path dir = Files.createDirectory(IOUtils.resolveFilePath("dir"));
-        
+
         PasteException exception =
           assertThrows(PasteException.class, () -> pasteApp.run(new String[]{"dir"}, System.in, System.out));
-        
+
         Files.delete(dir);
         assertTrue(exception.getMessage().contains(ERR_IS_DIR));
-        
+
     }
-    
-    
+
+
     /**
      * ‘paste - -’ causes input to stdin to be printed twice such that if the user inputs
      * ‘a’ on a line and ‘b’ on the following line then terminates the command, the output generated is:
@@ -359,24 +363,24 @@ public class TeamUBugs {
     @Test
     @DisplayName("Bug #11")
     public void testPasteTwoStdinArgs() throws IOException, PasteException {
-        
+
         // set curred dir to the folder with test assets
         Environment.currentDirectory += StringUtils.fileSeparator() + "dummyTestFolder"
           + StringUtils.fileSeparator() + "PasteTestFolder";
-        
+
         input = new ByteArrayInputStream(Files.readAllBytes(IOUtils.resolveFilePath("pasteFile1.txt")));
-        
+
         PasteApplication pasteApp = new PasteApplication();
-        
+
         pasteApp.run(new String[]{"-", "-"}, input, output);
-        
+
         assertEquals(new String(Files.readAllBytes(IOUtils.resolveFilePath("pasteFile1-2cols.txt")))
             + STRING_NEWLINE,
           output.toString());
-        
+
     }
-    
-    
+
+
     /**
      * File not found when using ‘paste - file’ (pasting files with stdin) because filenames
      * are not resolved against current directory
@@ -384,24 +388,24 @@ public class TeamUBugs {
     @Test
     @DisplayName("Bug #12")
     public void testPasteOneFileOneStdinArgs() throws PasteException, IOException {
-        
+
         // set curred dir to the folder with test assets
         Environment.currentDirectory += StringUtils.fileSeparator() + "dummyTestFolder"
           + StringUtils.fileSeparator() + "PasteTestFolder";
-        
+
         input = new ByteArrayInputStream(Files.readAllBytes(IOUtils.resolveFilePath("FILE2")));
-        
+
         PasteApplication pasteApp = new PasteApplication();
-        
+
         pasteApp.run(new String[]{"pasteFile1.txt", "-"}, input, output);
-    
+
         assertEquals(new String(Files.readAllBytes(IOUtils.resolveFilePath("pasteFiles1and2.txt")))
             + STRING_NEWLINE,
           output.toString());
-    
+
     }
-    
-    
+
+
     /**
      * NoSuchElementException exposed to user when the user attempts to redirect output without
      * a destination file (‘cmd args >’, e.g. ‘echo hello >’).
@@ -410,28 +414,28 @@ public class TeamUBugs {
     @Test
     @DisplayName("Bug #13")
     public void testOutputRedirFailsNoDest() {
-        
+
         IORedirectionHandler ioRedirHandler = new IORedirectionHandler(
           Arrays.asList("echo", "hello", ">"),
           System.in,
           System.out,
           new ArgumentResolver()
         );
-        
+
         ShellException shellException =
           assertThrows(ShellException.class, () -> ioRedirHandler.extractRedirOptions());
-        
+
         assertTrue(shellException.getMessage().contains(ERR_SYNTAX));
     }
-    
-    
+
+
     /**
      * Bug #14
      * Cannot finish executing paste command with stdin input without terminating the shell
      * 'paste -' only terminates on EOF, but EOF terminates the entire shell application.
      */
-    
-    
+
+
     /**
      * Find with globs causes an unexpected error: ‘Paths must precede -name’.
      * The problem should be that you can only search for a single name
@@ -447,20 +451,20 @@ public class TeamUBugs {
             assertMsgContains(e, "find: Only one filename is allowed");
         }
     }
-    
-    
+
+
     /**
      * Find with quoted glob causes inexplicable exception ‘Dangling meta character '*' near index 0’
      */
     @Test
     @DisplayName("Bug #16")
     public void testFindGlob() {
-        
+
         Environment.currentDirectory = ORIGINAL_DIR
           + StringUtils.fileSeparator() + "dummyTestFolder"
           + StringUtils.fileSeparator() + "IntegrationTestFolder"
           + StringUtils.fileSeparator() + "GlobbingFolder";
-        
+
         try {
             shell.parseAndEvaluate("find ./ -name \"*\"", output);
             assertEquals("./dir"
@@ -470,6 +474,31 @@ public class TeamUBugs {
         } catch (Exception e) {
             fail(e.getMessage());
         }
+    }
+
+    /**
+     * Try find command with multiple flags
+     * The command works if last flag is -name. If any other flag is last, wrong message is displayed.
+     */
+    @Test
+    @DisplayName("Bug #17")
+    public void testFindWithMultipleFlags() {
+        FindApplication findApplication = new FindApplication();
+        Exception expectedException = assertThrows(FindException.class, () -> findApplication.run(new String[]{"A",
+            "-name", "-i", "test"}, System.in, output));
+        assertTrue(expectedException.getMessage().contains(WRONG_FLAG_SUFFIX));
+    }
+
+    /**
+     * Try find command with multiple file names
+     * The command displays a wrong error message 
+     */
+    @Test
+    @DisplayName("Bug #18")
+    public void testFindWithMultipleFileNames() {
+        FindApplication findApplication = new FindApplication();
+        Exception expectedException = assertThrows(FindException.class, () -> findApplication.run(new String[]{"Test-folder-2", "-name", "Test-folder-2-2", "textfile.txt"}, System.in, output));
+        assertTrue(expectedException.getMessage().contains(MULTIPLE_FILES));
     }
 
     /**
@@ -516,4 +545,18 @@ public class TeamUBugs {
             fail(e.getMessage());
         }
     }
+
+    /**
+     * Try find command with wrong flag suffix
+     * The error thrown is wrong. It shows no file is provided where as the wrong flag is provided.
+     */
+    @Test
+    @DisplayName("Bug #32")
+    public void testFindWithWrongFlagSuffix() {
+        FindApplication findApplication = new FindApplication();
+        Exception expectedException = assertThrows(FindException.class, () -> findApplication.run(new String[]{"A",
+            "-i", "test"}, System.in, output));
+        assertTrue(expectedException.getMessage().contains(WRONG_FLAG_SUFFIX));
+    }
+
 }
